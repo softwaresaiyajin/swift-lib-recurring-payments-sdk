22ed4e830a6de3d256cdeb068f7c8c99a16acceb
Vytautas Gimbutas <v.gimbutas@paysera.com>
2020-09-01 23:22:47 +0300

    Bumped SDKs versions

--- Package.swift
+++ Package.swift
@@ -8,8 +8,8 @@ let package = Package(
         .library(name: "PayseraRecurringPaymentsSDK", targets: ["PayseraRecurringPaymentsSDK"]),
     ],
     dependencies: [
-        .package(name: "PayseraCommonSDK", url: "https://github.com/paysera/swift-lib-common-sdk", from: "3.0.0"),
-        .package(name: "PayseraAccountsSDK", url: "https://github.com/paysera/swift-lib-accounts-sdk", from: "6.0.0")
+        .package(name: "PayseraCommonSDK", url: "https://github.com/paysera/swift-lib-common-sdk", from: "3.0.1"),
+        .package(name: "PayseraAccountsSDK", url: "https://github.com/paysera/swift-lib-accounts-sdk", from: "6.0.2")
     ],
     targets: [
         .target(
